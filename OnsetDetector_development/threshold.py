3fbe0459c6a31131a8de5f94d5cc9f576938f353
keziah55 <19816994+keziah55@users.noreply.github.com>
2019-10-22 20:26:34 +0100

    adding wav file

--- OnsetDetector_development/threshold.py
+++ OnsetDetector_development/threshold.py
@@ -13,22 +13,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import soundfile as sf
 import os
-from save_plot import SavePlot
 
 sns.set_style('whitegrid')
 
 fname = 'dre48'
 
-user = os.path.expanduser('~')
-project = os.path.join(user, 'onsets', 'hsj')
-savepath = os.path.join(project, 'Visualisation', 'onset_detection', 
-                        'threshold_backtrack')
-
-file = os.path.join(project, 'Data', fname + '.wav') #'Sufi_songs', 
+file = os.path.join('data', fname + '.wav')
 
 audio, sr = sf.read(file)
-
-# audio = audio[:int(sr*15)]
 
 onsets = np.array([8.47467, 8.98133, 9.76])
 
@@ -47,19 +39,12 @@ method = DetectorBank.runge_kutta
 f_norm = DetectorBank.freq_unnormalized
 a_norm = DetectorBank.amp_unnormalized
 d = 0.0001
-gain = 50
+gain = 100
 
 threshold = 0.8 #  0.3 # 
 
 f = np.array([440*2**(-2/12)])
 
-savename = '391Hz_response_dream_d1e-4_0825_1025s_thr' #'{:.0f}Hz'.format(np.round(f0))
-#savename = '391Hz_response_dream_d1e-4'
-
-sp = SavePlot(False, os.path.join(savepath, savename+'.pdf'),
-              mode='quiet')
-# sl = SaveLegend(os.path.join(savepath, '{:.0f}Hz_legend.pdf'
-#                              .format(np.round(f0))), mode='quiet')
 
 bandwidth = np.zeros(len(f))
 det_char = np.array(list(zip(f, bandwidth)))
@@ -90,13 +75,6 @@ plt.axhline(threshold, color='green', linestyle='dashed')
 #plt.axvline(8.925, color='lime')
 #plt.axvline(onsets[which], color='red', linestyle='--')
 
-# sl.save(labels, colours=c, figsize=(3,3), ncol=2)
-#
-# print('Saved legend...')
-#
-# plt.title('{:.0f} to {:.0f}Hz, centre {:.0f}Hz'.format(np.round(f[0]),
-#           np.round(f[-1]),
-#           np.round(f0)))
 plt.grid(True)
 ax = plt.gca()
 plt.xlabel('Time(s)')
@@ -106,10 +84,5 @@ ax.set_ylabel('|z|', labelpad=10, rotation='horizontal')
 #plt.xlim(ts0, ts1)
 plt.yticks(np.linspace(0, 1.6, 9))
 
-sp.plot(plt)
-
-print('Saved plot')
-
-#print('Threshold exceeded at:')
-#for t in thr_exc:
-#    print('    {:.3f} seconds'.format(t/sr))
+plt.show()
+plt.close()
